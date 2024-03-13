4916350707ad4546c66b9fd98c52362d66e9d634
dutt.mistry <dutt.mistry@cybercom.in>
2024-03-13 15:43:44 +0530

    Changes of NFC

--- src/containers/Write.js
+++ src/containers/Write.js
@@ -7,7 +7,7 @@ const Write = () => {
       const ndef = new window.NDEFReader();
       // This line will avoid showing the native NFC UI reader
       await ndef.scan();
-      await ndef.write({ records: [{ recordType: "text", data: message }] });
+      await ndef.write({ records: [{ recordType: "url", data: message }] });
       alert(`Value Saved!`);
     } catch (error) {
       alert(error);
