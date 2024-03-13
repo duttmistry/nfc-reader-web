1c5c255491f6a6ced22fe4b7b340a66a7158ce94
duttmistry <dutt.cybercom@gmail.com>
2024-03-13 22:20:13 +0530

    Changes in Nfc

--- src/components/Writer/Writer.js
+++ src/components/Writer/Writer.js
@@ -2,48 +2,75 @@ import React, { useContext } from "react";
 import "./Writer.css";
 import Save from "../SVGs/save";
 import { ActionsContext } from "../../contexts/context";
+import { useFormik } from "formik";
+import {
+  FormControl,
+  FormControlLabel,
+  Radio,
+  RadioGroup,
+  TextField,
+} from "@mui/material";
 
 const Writer = ({ writeFn }) => {
   const [message, setMessage] = React.useState("");
+  const { values, handleChange, handleSubmit, setValues } = useFormik({
+    initialValues: {
+      message: "",
+      type: "text",
+    },
+    onSubmit: () => {
+      setMessage(values);
+      console.log("values: ", values);
+      console.log("message: ", message);
+      // e.preventDefaultuseFormik();
+      writeFn(message?.message);
+      setMessage("");
+      setActions({
+        scan: "scanned",
+        write: message,
+      });
+    },
+  });
   const { setActions } = useContext(ActionsContext);
-  const onSave = (e) => {
-    e.preventDefault();
-    writeFn(message);
-    setMessage("");
-    setActions({
-      scan: "scanned",
-      write: message,
-    });
-  };
 
   return (
     <>
-      <form onSubmit={onSave}>
+      <form onSubmit={handleSubmit}>
         <div className="writer-container">
-          <label
-            class="form-check-label"
-            for="text"
-            onClick={() => setMessage({ ...message, type: "text" })}
-          >
-            Text
-          </label>
-          <input class="form-check-input" type="radio" name="text" id="text" />
-          <label
-            class="form-check-label"
-            for="url"
-            onClick={() => setMessage({ ...message, type: "url" })}
-          >
-            Url
-          </label>
-          <input class="form-check-input" type="radio" name="url" id="url" />
-          <input
+          <FormControl>
+            <RadioGroup
+              aria-labelledby="demo-radio-buttons-group-label"
+              defaultValue="text"
+              name="type"
+            >
+              <FormControlLabel
+                // value={values?.type}
+                value="text"
+                control={<Radio />}
+                onChange={handleChange}
+                label="Text"
+                name="type"
+              />
+              <FormControlLabel
+                // value={values?.type}
+                // value={values?.type}
+                value="url"
+                control={<Radio />}
+                label="Url"
+                name="type"
+                onChange={handleChange}
+              />
+            </RadioGroup>
+          </FormControl>
+          <TextField
             type="text"
             placeholder="Enter Message..."
-            value={message?.message}
-            onChange={(e) =>
-              setMessage({ message: e.target.value, ...message })
-            }
-          ></input>
+            name="message"
+            // value={message?.message}
+            onInput={(e) => {
+              setValues({ message: e.target.value, type: values?.type });
+            }}
+          />
           <button className="btn" type="submit">
             <Save />
             Save
